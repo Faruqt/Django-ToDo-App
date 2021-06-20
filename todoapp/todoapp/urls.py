3f5c9a00b9cd045257cd35ad00063fb1120f7709
Faruq <faruqabdulsalam@yahoo.com>
2021-06-21 00:28:06 +0100

    Update static root

--- todoapp/todoapp/urls.py
+++ todoapp/todoapp/urls.py
@@ -31,5 +31,5 @@ urlpatterns = [
     path('logout/', logout_view, name='logout'),
   ]
 
-# urlpatterns += static (settings.STATIC_URL, document_root = settings.STATIC_ROOT)
-# urlpatterns += static (settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static (settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+urlpatterns += static (settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
