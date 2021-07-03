11200cb9964ca57905425699657a153f7d32246d
Faruq <faruqabdulsalam@yahoo.com>
2021-07-03 04:04:25 +0100

    Solve login character issue

--- login_registration/forms.py
+++ login_registration/forms.py
@@ -1,23 +1,35 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 class CustomUserCreationForm(UserCreationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={
         "class": "input",
-        "type": "text",
     }))
 
-    password1 = forms.CharField(widget=forms.TextInput(attrs={
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
         "class": "input",
-        "type": "password",
     }))
 
-    password2 = forms.CharField(widget=forms.TextInput(attrs={
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
         "class": "input",
-        "type": "password",
     }))
 
     class Meta:
         model = User
         fields = ['username', 'password1', 'password2']
+
+    def clean_username(self):
+        data = self.cleaned_data['username']
+        return data.lower()
+
+
+class LoginForm(AuthenticationForm):
+ 
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+    def clean_username(self):
+        data = self.cleaned_data['username']
+        return data.lower()
--- login_registration/views.py
+++ login_registration/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, LoginForm
 # Create your views here.
 
 def logout_view(request):
@@ -10,9 +10,9 @@ def logout_view(request):
 
 def login_view(request):
     error_message = None
-    form = AuthenticationForm()
     if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
+        form = LoginForm(data=request.POST)
+
         if form.is_valid():
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
@@ -23,8 +23,8 @@ def login_view(request):
                     return redirect(request.GET.get('next'))
                 else:
                     return redirect('todolist:my_todos')
-        else:
-            error_message = 'oops ... something went wrong'
+    else:
+        form = LoginForm()
 
     context = {
         'form': form,
@@ -53,3 +53,5 @@ def registerUserView(request):
 
     context= {'form':form}
     return render(request,'auth/register.html', context)
+
+    
